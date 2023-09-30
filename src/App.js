f32cd54e4c6187141ae1629035db977b065b2da3
rociohleija <137197779+rociohleija@users.noreply.github.com>
2023-09-30 07:56:41 -0500

    Added footer to my App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
-     
+     Weather App
+     <footer>
+      This project was coded by{" "}
+      <a href="https://astonishing-pixie-d099a0.netlify.app/" target="_blank">
+        Rocio Hernandez
+      </a>{" "}
+      and is{" "}
+      <a
+      href="https://github.com/rociohleija/Weather-React-App" target="_blank">
+        open-source on GitHub
+      </a>
+     </footer>
     </div>
   );
 }
