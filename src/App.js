c5ed37fab5c3be3ccd1643adcd32c39d817c42d4
rociohleija <137197779+rociohleija@users.noreply.github.com>
2023-09-30 13:24:07 -0500

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
      <h1>Weather App</h1>
      <footer>
        This project was coded by{" "} 
@@ -13,6 +14,7 @@ export default function App() {
        and is{" "} 
        <a href="https://github.com/rociohleija/Weather-React-Application">open-source on GitHub.</a>
      </footer>
+     </div>
     </div>
   );
 }
