16fe2c7ea99fbf2121f4592829315e1ed57e304a
rociohleija <137197779+rociohleija@users.noreply.github.com>
2023-09-30 10:48:43 -0500

    Remove footer

--- src/App.js
+++ src/App.js
@@ -6,20 +6,6 @@ export default function App() {
   return (
     <div className="App">
      Weather App
-     <footer>
-      This project was coded by{" "} <a href="https://astonishing-pixie-d099a0.netlify.app/" target="_blank">Rocio Hernandez</a>  and is{" "}
-      <a href="https://github.com/rociohleija/Weather-React-App" targer="_blank">
-        open source on GitHub
-      This project was coded by{" "}
-      <a href="https://astonishing-pixie-d099a0.netlify.app/" target="_blank">
-        Rocio Hernandez
-      </a>{" "}
-      and is{" "}
-      <a
-      href="https://github.com/rociohleija/Weather-React-App" target="_blank">
-        open-source on GitHub
-      </a>
-     </footer>
     </div>
   );
 }
