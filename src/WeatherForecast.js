3b919da70d3911174f1abb4f7e0dc3c28fc857d6
rociohleija <137197779+rociohleija@users.noreply.github.com>
2023-10-04 11:41:24 -0500

    Added a new component Weather Forecast Day with real data

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,36 +1,36 @@
-import React from 'react';
-import WeatherIcon from './WeatherIcon';
+import React, { useState } from 'react';
 import axios from 'axios';
+import './WeatherForecast.css';
+import WeatherForecastDay from './WeatherForecastDay';
 
 export default function WeatherForecast(props) {
+    let [loaded, setLoaded] = useState(false);
+    let [forecast, setForecast] = useState(null);
+
     function handleResponse(response) {
-        console.log(response.data)
+        setForecast(response.data.daily);
+        setLoaded(true);
     }
+
+    if(loaded) {
+        return (
+        <div className ="WeatherForecast">
+            <div className ="row">
+                <div className ="col">
+                    <WeatherForecastDay data={forecast[0]} />
+                </div>
+            </div>
+        </div>
+        );
+
+    } else {
+     
     let apiKey = "3dce9b1c66837262a25b3f448d354a76";
     let longitude = props.coordinates.lon;
     let latitude = props.coordinates.lat;
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`;
     
     axios.get(apiUrl).then(handleResponse);
-    return (
-        <div className ="WeatherForecast">
-            <div className ="row">
-                <div className ="col">
-                    <div className ="WeatherForecast-day">
-                        Thu
-                    </div>
-                    <WeatherIcon code ="01d" size={36}/>
-                    <div className ="WeatherForecast-temperatures">
-                        <span className ="WeatherForecast-temperature-max">
-                            19°
-                        </span>
-                        <span className ="WeatherForecast-temperature-min">
-                            10°
-                        </span>
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
- 
+    ;
+    }
 }
